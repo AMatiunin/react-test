48e028169397c5b96d80e02a902c0e9f4766d714
Anton Matiunin <anton.matiunin@onix-systems.com>
2022-12-20 14:39:12 +0200

    add button

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,7 @@ export default function Homepage() {
         <div className='naming'>Name: test-react-application</div>
         <div className='description'>Description: this is a test project for onyx-academy</div>
         <div className='workspace'>Workspace: initial homepage for further skill improving</div>
+        <button>Button</button>
         </body>
     )
 }
